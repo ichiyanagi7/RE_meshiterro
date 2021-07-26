c3aa9c8ea8a2396f6288b52604a5310230783757
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-26 03:43:56 +0000

    [finish]4-3復習

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dcf2b2d4b061206d56068b152312f524552907df0e99bebea4db5a4a6106d49ba2301e83bb87aa7118d1ffbffe2e44c77d7adaa9a63bccfd9556e96251ac129c'
+
